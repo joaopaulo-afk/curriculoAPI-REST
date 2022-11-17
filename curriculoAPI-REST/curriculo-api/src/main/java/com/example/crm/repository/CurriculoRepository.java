965994e10b18ffd73fb2e3956f1c80d084efdbcb
joaopaulo-afk <85767300+joaopaulo-afk@users.noreply.github.com>
2022-11-17 15:09:08 -0300

    Commit by JP

---
+++ curriculoAPI-REST/curriculo-api/src/main/java/com/example/crm/repository/CurriculoRepository.java
@@ -0,0 +1,9 @@
+package com.example.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.crm.model.Curriculo;
+
+public interface CurriculoRepository extends JpaRepository<Curriculo, Long> {
+
+}
